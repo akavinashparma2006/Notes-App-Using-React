ad16a8e593b0bd1ac33880c1e4ad3f15729986e3
Avinash Parmar <akavinashparmar@example.com>
2026-06-17 12:54:46 +0530

    added delete style

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { MdDelete } from "react-icons/md";
 
-function Card({ title, desc }) {
+function Card({ title, desc, deleteNote }) {
   return (
     <div className='card'>
+      <div className='del' onClick={()=>deleteNote(title)}>
+      <MdDelete /></div>
       <div className='title'>{title}</div>
       <div className='desc'>{desc}</div>
     </div>
--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,48 @@
 import './App.css'
 import Navabar from '../components/Navabar.jsx'
 import { useState } from 'react'
+
 import Card from '../components/card.jsx'
 
+
 function App() {
-  const [Notes, setNotes] = useState([])
+
+  const [Notes, setNotes] = useState(() => {
+    const localNotes = localStorage.getItem('notes')
+    return localNotes ? JSON.parse(localNotes) : []
+  })
+
 
   const [currentNote, setcurrentNote] = useState({ title: "", desc: "" })
 
+
+
+
+
+  
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    setNotes([...Notes, currentNote])
-    setcurrentNote({title:"",desc:""})
+
+    const updatedNotes = [...Notes, currentNote]
+    setNotes(updatedNotes)
+    setcurrentNote({ title: "", desc: "" })
+
+    localStorage.setItem("notes", JSON.stringify(updatedNotes))
   }
 
+
+
+  const deleteNote = (titleToDelete) => {
+    const updatedNotes = Notes.filter((item) => item.title != titleToDelete)
+    setNotes(updatedNotes)
+    localStorage.setItem('notes', JSON.stringify(updatedNotes))
+  }
+
+
+
   const handleChange = (e) => { setcurrentNote({...currentNote, [e.target.name]: e.target.value}) }
+  
 
 
   return (
@@ -45,7 +73,7 @@ function App() {
         <h2>Your Notes</h2>
         <div className='container'>
         {Notes && Notes.map(note => {
-          return <Card title={note.title} desc={note.desc}/>
+          return <Card title={note.title} deleteNote={deleteNote} desc={note.desc}/>
         })}
         </div>
       </section>
